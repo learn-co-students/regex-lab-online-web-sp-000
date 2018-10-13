9f3c9d909c4dcf7e5a64f69e61f8de749ae1d128
Sam Tamburri <tamburrisam@gmail.com>
2018-10-13 16:39:11 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+# (/[aeiou]/)
+
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   if text.scan(/^[A-Z]/).any?&&text.scan(/[,.?!]$/).any?
+    return true
+  else
+    return false
+  end
 end
 
-def valid_phone_number?(phone)
-
+ def valid_phone_number?(phone)
+   arr = phone.scan(/[0-9]/)
+  if arr[9] == nil
+    return false
+  else
+    return true
+  end
 end
