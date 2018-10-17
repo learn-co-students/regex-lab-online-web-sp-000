32fd5359fccee576f87e43410c8afc827f45f430
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-10-17 21:49:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,12 @@
 def starts_with_a_vowel?(word)
 
+if word.match(/\A[aeiouAEIOU]/) != nil 
+  return true 
+else 
+  return false
+end
+
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
