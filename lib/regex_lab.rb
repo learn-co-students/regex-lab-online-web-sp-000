c42b4efb2ac7fca2321bab218e620511238c1d13
Aglae Verduzco <aglae.cv@gmail.com>
2018-11-11 00:17:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,17 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\A[un]+S[ing]\z/)
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*[\.,:;]$/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/^\d{3}.\d{3}.\d{4}$/)
 end
