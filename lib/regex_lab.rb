baf10da9469f94d639497f6ea8c7d5ec2109e1d3
Alison Reilly <reillyamr@gmail.com>
2018-12-03 17:46:12 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
-def starts_with_a_vowel?(word)
+def starts_with_a_vowel?(word) #works
 
+  if word.match(/\b[aeiouAEIOU]/) != nil 
+    return true 
+  else 
+    return false 
+  end 
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(text) #works
+  text.scan(/\b+un+\w+ing/)
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(text) #works
+  text.scan(/\b+\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+def first_word_capitalized_and_ends_with_punctuation?(text) #works
+  
+  if text.match(/\A[A-Z]/) && text.match(/\b[!.?]$/)
+    return true
+  else
+    return false
+  end
 
 end
 
 def valid_phone_number?(phone)
+ if phone.match(/\d{10}$|^[0-9)(-]{13}$|^[0-9 ]{12}$|^[0-9)(]{12}$/)
+   return true
+ else
+   return false
 
+ end
 end
