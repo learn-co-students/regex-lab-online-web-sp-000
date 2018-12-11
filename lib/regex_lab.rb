bb241365a753362b36a44c62c1b99bff97abf321
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-11 17:26:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,23 @@
 def starts_with_a_vowel?(word)
- matches = word.match(/\A[aAeEiIoOuU]/) ? true : false
+word.match(/\A[aAeEiIoOuU]/) ? true : false
 end
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-matches = text.scan(/un+\w+ing/)
-return matches
+text.scan(/un+\w+ing/)
+
 end
 
 def words_five_letters_long(text)
-matches = text.scan(/\b\w{5}\b/)
-return matches
+text.scan(/\b\w{5}\b/)
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-matches = text.match(/\A[A-Z].*\./) ? true : false
+text.match(/\A[A-Z].*\./) ? true : false
 end
 
 def valid_phone_number?(phone)
-matches = phone.match(/\d{3}\S?\s?\d{3}\S?\s?\d{4}/)
-return matches
+phone.match(/\d{3}\S?\s?\d{3}\S?\s?\d{4}/)
+
 end
