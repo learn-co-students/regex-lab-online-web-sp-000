20ce4c80d9d44fa8eeb5a8eb4cbe05ddda2423bb
Diana Badas <diana.badas@gmail.com>
2018-12-13 23:53:19 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,12 @@
 def starts_with_a_vowel?(word)
-
+  !!(word[0] =~ /[aeiou]/i)
 end
+=begin
+The regex reads, "match a vowel, case indifferently (/i)". !! converts a thruthy value to true and a falsy value (nil or false) to false:
+
+!!0 = !(!0) = !(false) = true
+!!nil = !(!nil) = !(true) = false
+=end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
