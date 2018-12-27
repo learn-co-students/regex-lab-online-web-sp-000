1c216da71d06af4871e251721f8516ede382d1fc
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-27 05:43:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  if word[0].match(/[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\b/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/[\d/)
 end
