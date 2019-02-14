245a20d17506e9551471fb47cac4dd527aa87eaa
Tyler Hunt <tyhunt04@gmail.com>
2019-02-14 01:06:07 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aAeEiIoOuU]+[\w]*\b/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) && text.match(/[^\s\w]$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/)
+    true
+  else
+    false
+  end
 end
