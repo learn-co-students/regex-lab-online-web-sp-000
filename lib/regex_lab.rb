713b9c73cc6001987aa3a76f7fd78eaaf61411f0
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-03 16:17:04 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  if word.match (/^aeiou/) 
+    true 
+  else 
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +20,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+
 
 end
