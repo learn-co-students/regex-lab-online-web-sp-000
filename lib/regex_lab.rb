5bd5d3853080cf41f7e63765455e1520bfbf02f6
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-06 20:29:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  if word.match(/[^aeiou]/) 
+  if word.match(/^[aeiouAEIOU]/) 
     true 
   else 
     false 
@@ -8,18 +8,28 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w*un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+     text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/[A-Z]\W/)
+     true
+    else 
+     false 
+    end 
 end
 
-def valid_phone_number?(phone)
 
+def valid_phone_number?(phone)
+  if phone.match(/\d\S/)
+    #binding.pry 
+    true 
+  else 
+    false 
+  end   
 
 end
