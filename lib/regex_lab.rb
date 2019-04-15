502c893c584553967b7926cd0666385b4531333c
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-15 16:46:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/^[\D]?\d{3}[\D]?\d{3}[\D]?\d{4}$/) != nil
+  if phone.match(/^[\D]?\d{3}[\D]?\d{3}[\D]?\d{4}[\D]?$/) != nil
     true
   else
     false
