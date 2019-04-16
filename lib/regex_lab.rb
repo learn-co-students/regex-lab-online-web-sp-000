235a13078b6ebb5372608d60863cf0ef696938a8
Danielle Bergbower <dkw5011@gmail.com>
2019-04-16 03:33:26 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,9 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if phone.scan(/\b\S\w\d{0,9}\b/)
-   return true
- else
+   if phone.scan(/\d/).length == 10
+     return true
+  else
    false
  end
 end
