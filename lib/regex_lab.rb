1d0863f3d429c3dbaa4041ce36334c483bf3f244
Oana Berentan <oana.berentan@yahoo.com>
2019-04-23 21:31:15 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/^[aeiouAEIOU]+\w/)
+  return true
+  else
+  return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b+\w{5}+\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
+  if text.match(/^[A-Z]+\b[.!?]) then
+      return true
+    else
+      return false
+    end
 
 def valid_phone_number?(phone)
+
 
 end
