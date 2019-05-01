827a4a25a7eda2577ae2464682c75b0fd45e2d26
Ramon Chavez <rachavez1974@gmail.com>
2019-05-01 11:22:43 -0400

    Finish regex lab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,29 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  # phone_valid = []
-
-  # for p in phone do
-  #   if p.match(/(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/)
-  #     phone_valid.push(true)
-  #   else
-  #     phone_valid.push(false)
-  #   end
-  # end
-
-  # if phone_valid.all? {|p| p == true}
-  #   return true
-  # else
-  #   return false
-  # end
-
   phone.match(/(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/)
-  
-  
 end
 
-# # valid_numbers = ["1234567890", "3456789087"]
-#valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
-# valid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
-#puts valid_phone_number?(valid_numbers)
 
