50bae441e7b1bdf6926ca8838132ef9369a9b180
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-13 18:20:52 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,10 @@
 def starts_with_a_vowel?(word)
-   if word.match(/\b[AEIOUaeiou]/)
-   end
- end
+    if word == word.match(/\b[AEIOUaeiou][a-z]*\b/)
+    end
+end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(un[a-z]+ing)/)
 end
 
 def words_five_letters_long(text)
