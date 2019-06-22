77e2d9381ad52fde86402f8bf3e562222145f4dd
Chantel Lowe <chantellowe17@gmail.com>
2019-06-22 05:31:31 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,48 @@
-def starts_with_a_vowel?(word)
+#aeiou
 
+def starts_with_a_vowel?(word)
+  if word.match(/\A[aeiouAEIOU]/) != nil
+    true
+  else
+    false
+  end
 end
+
+
+#un...ing
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
+
+
+
+#exact length, 5 letters
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
+
+
+
+#starts w/ capital letter, ends w/ punctuation
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\.$/) == nil
+    false
+  else
+    true
+  end
 end
 
-def valid_phone_number?(phone)
 
+
+#valid phone # regardless of formatting
+
+def valid_phone_number?(phone)
+  if phone.match(/\(?([\d]{3})\)?([ -]?)\(?([\d]{3})\)?([ -]?)\(?([\d]{4})\)?/) == nil
+    false
+  else
+    true
+  end
 end
