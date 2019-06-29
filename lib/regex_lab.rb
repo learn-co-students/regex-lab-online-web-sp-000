e78b6cd5dd9604df48a67f853b2b13f621133013
Monique Vitche <mvitche7@gmail.com>
2019-06-29 17:58:43 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/[aeiou]/i)
+    true
+  elsif word.match([b-d,f-h,j-n,p-t,v-z])
+      false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/[A-Z].*(\.|\?|!)$/)
+    true
+  else
+    false
 end
 
 def valid_phone_number?(phone)
-
+# has to have 10 digits, ignore spaces, hyphens and parentheses.
+  if phone.match(/.*?\d{3}.*?\d{3}.*?\d{4}$/)
+    true
+  else
+    false
 end
