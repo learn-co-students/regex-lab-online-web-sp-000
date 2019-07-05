fa94a0ffc384de5cee8bf73c48b980fa34dc0e0a
Alice Brunel <alc.brunel@gmail.com>
2019-07-05 17:06:30 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/\b[aeiouAEIOU]\w*/)
+    return true
+   else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/[A-Z]\w*\b[^\s']/)
+    return true
+  else
+    return false
+  end
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number?(phone_numbers)
+  invalid = phone_numbers.scan(/[a-zA-Z]/)
+  return invalid[0] == nil
 end
