ce0c31b8d580b7865cecd8ee19fbd845257c6ed0
github username <github email address>
2019-08-30 15:09:45 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,25 +1,20 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  "aeiou"word.match(/\w+[aeiou]/) ? true
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
-# def match 
-#   puts "The rain in Spain lies mainly in the plain".match(/\w+ain/)
-# end
-# match()
-
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
