e3b994b0bfa7ac8eb560b904d44809bbb5fa263b
Blythe Pollard <blytheepollard@gmail.com>
2019-09-03 23:06:39 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,14 @@
 def starts_with_a_vowel?(word)
- word = "afoot Excellent incredible Absolute unreal Inconceivable octopus"
+ word = "afoot Excellent incredible Absolute unreal Inconceivable octopus chandoo Crenel crooked Flimsy folksy Dog Hermit makeshift Palazzi sixfold Truth"
  if word.scan(/^[aeiou]/)
    true
+  else
+  false
  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/(^\un)/)
 end
 
 def words_five_letters_long(text)
