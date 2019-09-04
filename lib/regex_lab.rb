211d83778f6b303e708e6cac78678a7ebfdd7864
Blythe Pollard <blytheepollard@gmail.com>
2019-09-04 03:57:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,20 @@
 def starts_with_a_vowel?(word)
  word = "afoot Excellent incredible Absolute unreal Inconceivable octopus chandoo Crenel crooked Flimsy folksy Dog Hermit makeshift Palazzi sixfold Truth"
- if word.scan(/^[aeiou]/)
+ if word.scan(/\w+^[aeiouAEIOU]/)
    true
-  else
+  elsif word.match(/\w+^[^aeiouAEIOU]/)
   false
  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/(^\un)/)
+#<<<<<<< HEAD
+ "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/\b[un]\w+\b/)
+
 end
 
 def words_five_letters_long(text)
-
+  "extreme briny crepe parking snaps grouping snafu round dog be fork spoon".scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
