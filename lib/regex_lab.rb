ef1a6af401604160ce3ad2807b6e6f6356b09921
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-01 21:44:06 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
+require 'pry'
 def starts_with_a_vowel?(word)
+word.match? (/\b[AEIOUaeiou][a-z]*\b/)
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan (/un[a-z]*ing/)
 
 end
 
 def words_five_letters_long(text)
-
+text.scan (/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  # text.match? (/\b^[A-Z]\b/)
+  text.match? (/\b[A-Z_\-]\b/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan (/(\d+)-(\d+)-(\d+)/)
+  # phone.match? (/(\d+)-(\d+)-(\d+)/)
 end
