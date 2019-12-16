981532310a2cf9a769bfd6b6baf21e00e6be6f32
Will Carter <will.carter@fergusllc.com>
2019-12-16 22:53:23 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]+\w/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/\bun\w+\ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/)
+    return true
+  else
+    return false
+  end
 end
