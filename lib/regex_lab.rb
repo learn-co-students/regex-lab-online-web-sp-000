25352b67b0c71ad241c34b8c76d8053e56be1aae
Will Corcoran <corcoranwill@gmail.com>
2020-02-12 11:16:27 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/\b[aeiou]/i) ? true : false
+  word.match?(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+\.$/) ? true : false
+  text.match?(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match(/^[(]?\d{3}[)]?[-]?[\s]?\d{3}[-]?[\s]?\d{4}$/) ? true : false
+  phone.match?(/^[(]?\d{3}[)]?[-]?[\s]?\d{3}[-]?[\s]?\d{4}$/) ? true : false
 end
