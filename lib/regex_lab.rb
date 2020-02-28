be7b6a2e7e698fda95d91f612674a06484d9fd0c
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-28 17:35:01 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,20 +1,21 @@
 def starts_with_a_vowel?(word)
-  word.match(/\w+[aeiou]/)
+  word.match(/\w[aeiou]/)
     true 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\w+[A-Z]+./)
+    false 
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/(\d+){3}(\d+){3}(\d+){4}/)
 end
