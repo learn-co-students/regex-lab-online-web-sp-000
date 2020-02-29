e241dcb28392a7b3746788118a142e29d9f57393
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-29 17:19:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/\w[aeiou]/)
+  if word.match(/\w[aeiou]/)
     true 
+  else
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -8,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.grep(/\w{5}/)
+  text.match {|word| word = (/\w{5}/)}
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
