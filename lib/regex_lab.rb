b81944732364a86f17319a221cef40405c54fa7a
Jung Lee <lee.jung.s95@gmail.com>
2020-04-16 01:37:26 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]+\w/)
+  if word.match(/^[aeiouAEIOU]+\w+/)
     return true
   else
     return false
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un+\w+ing/)
+  text.scan(/\bun+\w+ing/)
 end
 
 def words_five_letters_long(text)
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
     return true
   else
     return false
