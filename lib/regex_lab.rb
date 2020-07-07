963752ee14f10a862d553ffbdbce1de3c3ee31cc
Claire Bullock <bullockcm13@gmail.com>
2020-07-07 17:59:34 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiou]|^[AEIOU]/)
+    return TRUE
+  else
+    return false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  array = text.split(" ")
+  array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  first_letter = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+    return TRUE
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
-end
+    if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+      return TRUE
+    else
+      return false
+    end
+  end
+  
