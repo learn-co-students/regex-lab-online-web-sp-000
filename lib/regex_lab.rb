bdffc84ae2399a663c2a4a08ee23cdb77084544a
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-25 23:32:01 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+  word.match?(/\b[aeiou]/i)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/[A-Z]\w+(\b\S\s)/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d{3}\D?\d{3}([\S\s]?)\d{4}/)
 end
