158450f2572cfd10cd1c0991c372bb72bde0f479
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-06 15:03:23 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/\b+[aeiouAEIOU]/)
+     return true
+   end
+   return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*[.]/)
+    return true
+  end
+  return false
 end
 
 def valid_phone_number?(phone)
+  result = phone.match(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/)
+  puts result
 
+  if phone.match(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/)
+    return true
+  end
+  return false
 end
