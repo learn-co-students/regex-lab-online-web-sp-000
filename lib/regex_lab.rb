357f57ab345e0f03c97f19fadf1705be774145ea
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-22 00:21:55 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,3 +1,4 @@
+require "pry" 
 def starts_with_a_vowel?(word)
 if word.match(/^[aeiouAEIOU]/)
   return true 
@@ -8,39 +9,48 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   value = []
-  sample = text.split(" ")
-  sample.each do |something|
-    value << something.scan(/[^un][$ing]/)
+  count = 0 
+  until count == 4 
+     text.scan(/[^un$ing]/)
+    count +=1 
+    value << text 
   end 
 end 
 
 
 
- def words_five_letters_long(words_string)    
-   count = 1
+ def words_five_letters_long(words_string)      
    words = []
-   while count <= 6
-      if words_string.scan(/\w{5}/)
+   count = 0 
+   until count == 6
+   words_string.grep (/^.\w{5}/);
+   #words_string.scan/(\w{5})/
+      if words_string.length = 5 
      words << words_string 
-   count +=1 
-  end 
-end 
-words 
+     count +=1 
+      else 
+      end 
+  return words  
 end
+end 
+
+
  
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  sample = []
-  value = text.scan/[^A-Z][$\.!?]/
-  sample << value  
-  sample 
+  if text.scan(/^[^A-Z$\.!?]/)
+  true 
+else text.scan (/^[a-z])
+  false 
 end 
 
 
 
-def valid_phone_number?(phone) 
+def valid_phone_number?(phone)
+  def valid_phone_number?(phone) 
   if phone.scan(/[\d+]/)
     true 
   elsif phone.scan.all?(/(\d+)/)
     false   
      end   
   end 
+ 
