6524e322e22aaa43127fbb58ad270eb804762eb4
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-23 00:28:47 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,25 +20,12 @@ end
 
  
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  # it "Returns true for text starting with a capital letter and ending with #puncutation
-  if text.scan(/[\w^A-Z$\.!?]/)
-  true 
-  # it "Returns false for text starting with an uncapitalized letter but ending #with puncutation"
- elsif text.scan(/[\w^a-z]$\.!?/)
-  # it "Returns false for text starting with a capital letter but ending without #puncutation"
-elsif text.scan(/\w[^A-Z]$\.!?/)
-  false 
-  #it "Returns false for text starting without a capital letter and ending #without puncutation"
-  elsif text.scan(/\w[^a-z]!$\.!?/)
-  #it "Returns false for text starting without a capital letter and ending #without puncutation"
-
-  false 
-  else text.scan(/\w[^a-z]!$\.!?/)
+   #text.match(/\w^A-Z$\.!?/) ? true : false
+    text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
 end 
-end 
+ 
 
-
-def valid_phone_number?(phone)
+def valid_phone_number?(phone) 
   if phone.scan(/[d+]{10}/)
     true 
     phone.scan.all?(/[\d+]/)
