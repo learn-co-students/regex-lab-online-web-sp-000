1821d2118a40db78cf46abc3481bc01c7779c445
Chris Baum <christopher.baum@valpo.edu>
2021-01-10 23:57:08 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,20 +10,11 @@ def words_five_letters_long(text)
   text.scan(/\b[a-z,A-Z]{5}\b/)
 end
 
-#text = "I stepped on a Corn Flake, now I'm a cereal killer."
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/\b[A-Z]\w*\b[.,!]/) != nil ? true : false
 end
-#puts "#{first_word_capitalized_and_ends_with_punctuation?(text)}"
 
-#phone = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134", "28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
-#phone = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
-def valid_phone_number?(phone)
-  result = Array.new
-  phone.each do |number|
-    new_number = number.scan(/[0-9]/)
-    result << (new_number.count == 10)
-  end
-  result.all?{|i| i == true}
+def valid_phone_number?(number)
+  new_number = number.scan(/[0-9]/)
+  new_number.count == 10
 end
-#puts "#{valid_phone_number?(phone)}"
