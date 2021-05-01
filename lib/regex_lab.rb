5be2f9b5429ed00ec4b7aff34b38595b6fc98da3
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-01 13:43:56 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,13 +9,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w[a-z]{4}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].+\W$/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/\d/).count == 10
 end
