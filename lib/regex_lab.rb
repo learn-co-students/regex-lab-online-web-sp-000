131af155a7905e782ab71b0c0e67aac2a4a33d62
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-05-15 01:45:08 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,7 +28,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/[\d].+[a-zA-Z]$/)
+  if phone.match(/[\d].+$[a-zA-Z]/)
     true
   else
     false
