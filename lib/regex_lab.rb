9fdf058a6f05a542a6d7278b83a31f88ea677c47
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-05-15 01:44:29 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,20 +9,28 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  binding.pry
-  name = []
-  
-
+  #binding.pry
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  #binding.pry
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  #text.match(/^[A-Z].+[\.?!]$/) ? true : false
+  if text.match(/^[A-Z].+[\.?!]$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/[\d].+[a-zA-Z]$/)
+    true
+  else
+    false
+  end
 end
